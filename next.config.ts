de005a1350f57235dd83afb1c70d14c534132ed0
katawutp <katawut.paramee@gmail.com.com>
2025-12-06 15:44:55 +0700

    npm install thread-stream@2.2.0

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   env: {
-//     DATABASE_URL: process.env.DATABASE_URL,
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-// };
-
-// module.exports = nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
